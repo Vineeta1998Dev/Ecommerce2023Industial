d4285a207d14637c440addee763c8da6888ed264
netMaster <kushwahavineeta969@gmail.com>
2023-05-11 21:12:11 +0530

    add type and cat6egory on customer side

---
+++ NewEcommerc/NewEcommerc/Models/MainPageData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewEcommerc.Models
+{
+    public class MainPageData
+    {
+        ECOMMERCEEntities db = new ECOMMERCEEntities();
+        public List<Mstr_categories> GetAllTypes()
+        {
+            List<Mstr_categories> lstCat = new List<Mstr_categories>();
+            lstCat = db.Mstr_categories.Where(x => x.Pid == 0).ToList();
+            return lstCat;
+        }
+        public List<Mstr_categories> GetAllCategoriesByPId(int id)
+        {
+            List<Mstr_categories> lstCat = new List<Mstr_categories>();
+            lstCat = db.Mstr_categories.Where(x => x.Pid == id).ToList();
+            return lstCat;
+        }
+    }
+}
